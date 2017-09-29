088058e4fbc1e77569cb0a267db4e1a781e7f7a7
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-09-29 11:26:40 -0400

    ZombieBotCoby

---
+++ zombieChatBot/src/zombieProgram/ZombieBotCoby.java
@@ -0,0 +1,45 @@
+package zombieProgram;
+
+public class ZombieBotCoby implements Topic {
+
+	private String[] keywords;
+	private String goodbyeWord;
+	private String secretWord;
+	private boolean chatting;
+	
+	public ZombieBotCoby() {
+		String[] temp = {"food","entertainment","Internet","videogames"};
+		keywords = temp;
+		goodbyeWord = "bye";
+		secretWord = "pug";
+	}
+	
+	@Override
+	public boolean isTriggered(String response) {
+		for(int i = 0; i < keywords.length; i++) {
+			if(ZombieBotMain.findKeyword(response, keywords[i], 0) >= 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void startChatting(String response) {
+		ZombieBotMain.print("Hey! It sounds like you and I have common interests! Let's talk some more!");
+		chatting = true;
+		while(chatting) {
+			response = ZombieBotMain.getInput();
+			if(ZombieBotMain.findKeyword(response, goodbyeWord, 0) >= 0) {
+				chatting = false;
+				ZombieBotMain.chatbot.startTalking();
+			}else {
+				if(ZombieBotMain.findKeyword(response, secretWord, 0) >= 0) {
+					ZombieBotMain.print("Oh my goodness! You guessed my favorite thing ever. We are friends now.");
+				} else {
+					ZombieBotMain.print("Huh. I don't really get you. Tell me something else?");
+				}
+			}
+		}
+	}
+}
