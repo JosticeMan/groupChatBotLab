0ea21960cf8edec3e32ec044147924e7ef987c48
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-10-03 11:47:38 -0400

    c

--- zombieChatBot/src/zombieProgram/ZombieBotCoby.java
+++ zombieChatBot/src/zombieProgram/ZombieBotCoby.java
@@ -3,17 +3,25 @@ package zombieProgram;
 public class ZombieBotCoby implements Topic {
 
 	private String[] keywords;
-	private String goodbyeWord;
+	private String[] userOptions;
+	private String[] goodbyeWords;
 	private String secretWord;
+	private boolean gameStarted = false;
 	private boolean chatting;
+	private int topicChosen;
+	private String playerChoice;
+	
 	
 	public ZombieBotCoby() {
 		String[] temp = {"social life","hobbies","person","people","games","technology"};
 		keywords = temp;
-		String[] gameChoice = {"rock","paper","scissors"};
-		String goodbyeWord= "bye";
+		String[] temp2 = {"rock","paper","scissors"};
+		userOptions = temp2;
+		String[] temp3 = {"bye","goodbye","later",};
+		goodbyeWords = temp3;
 		secretWord = "the walking dead";
 	}
+	
 	
 	@Override
 	public boolean isTriggered(String response) {
@@ -25,33 +33,79 @@ public class ZombieBotCoby implements Topic {
 		return false;
 	}
 	
+	
 	@Override
 	public void startChatting(String response) {
 		ZombieBotMain.print("Hey. Let's have a conversation urrr! As you long as you feed me brainssssss afterwards.");
 		chatting = true;
+		
 		while(chatting) {
 			response = ZombieBotMain.getInput();
-			if(ZombieBotMain.findKeyword(response, goodbyeWord, 0) >= 0) {
+			
+			//if(gameStarted) {
+			//	for(int i = 0; i < userOptions.length; i++) {
+			//		if(ZombieBotMain.findKeyword(response,userOptions[i], 0) >= 0) {
+			//			ZombieBotMain.print(getWinner(userOptions[i]));
+			//		}
+			//	}
+			//}
+			
+			if(isthereWord(goodbyeWords,response)) {
 				chatting = false;
 				ZombieBotMain.chatbot.startTalking();
-			}else {
-				for(int i = 0; i < keywords.length; i++) {
-					if(ZombieBotMain.findKeyword(response,keywords[i], 0) >= 0) {
-						ZombieBotMain.print(getResponse(i));
-					}
-				}
-				if(ZombieBotMain.findKeyword(response, secretWord, 0) >= 0) {
-					ZombieBotMain.print("I love that show! Durrrr my brothers eventually get what they want.");
-				} else {
-					ZombieBotMain.print("Errrr I don't get it. Can you say something that I can relate to?");
-				}
+			}
+				
+			if(isthereWord(keywords,response)) {
+				ZombieBotMain.print(getResponse(topicChosen));
+			}
+				
+			if(ZombieBotMain.findKeyword(response, secretWord, 0) >= 0) {
+				ZombieBotMain.print("I love that show! Durrrr my brothers eventually get what they want.");
+			} else {
+				ZombieBotMain.print("Errrr I don't get it. Can you say something that I can relate to?");
 			}
 		}
 	}
 	
-	public String playGame() {
-		return "I won!";
+	public boolean isthereWord(String[] list, String response) {
+		for(int i = 0; i < list.length; i++) {
+			if(ZombieBotMain.findKeyword(response, list[i], 0) >= 0) {
+				return true;
+			}
+			if(list == keywords) {
+				topicChosen = i;
+				return true;
+			}
+		}
+		return false;
 	}
+	//public String getWinner(String player) {
+		//1 is rock; 2 is paper, 3 is scissors
+		//int zombieChoice = (int) ((Math.random() * 3) + 1);
+		//playerChoice = player.toLowerCase();
+		//if(zombieChoice == 1 && playerChoice == "paper") {
+		//	return "I picked rock. You won.";
+		//}
+		//if(zombieChoice == 1 && playerChoice == "scissors") {
+		//	return "I picked rock. I win! Now let me eat your brain!";
+		//}
+		//if(zombieChoice == 2 && playerChoice == "rock") {
+		//	return "I picked paper. I win! Now let me eat your brain!";
+		//}
+		//if(zombieChoice == 2 && playerChoice == "scissors") {
+		//	return "I picked paper. You won.";
+		//}
+		//if(zombieChoice == 3 && playerChoice == "rock") {
+		//	return "I picked scissors. You won.";
+		//}
+		//if(zombieChoice == 3 && playerChoice == "paper") {
+		//	return "I picked scissors. I won! Now let me eat your brain!";
+		//}
+		//gameStarted = false;
+		//return "It's a tie!";
+	//}
+	
+	
 	public String getResponse(int keywordFound) {
 		if(keywordFound == 0) {
 			return "I don't...have a social life......all I do is sleepp.";
@@ -60,10 +114,11 @@ public class ZombieBotCoby implements Topic {
 			return "I like to eat BRAINS....durrrrr let me eat yours!";
 		}
 		if(keywordFound == 2 || keywordFound == 3) {
-			return "My favorite people are smart people, their brains are more delicious. And you can assume my least favorite people are dumb people";
+			return "My favorite people are uh smart people.....their brains are more delicious. Errrrr of course my least favorite people are....dumb people";
 		}
 		if(keywordFound == 4) {
-			return "I love games! Lets play rock, paper, scissors! Type in your choice";
+			gameStarted = true;
+			return "I love games. Lets play rock, paper, scissors! Type in your choice";
 		}
 		if(keywordFound == 5) {
 			return "Technology? We don't use technology! When I was alive the only phone that was out was the first Nokia.";
