5aa43d67d4a3e0686c14ae523fe43d402e034d20
Coby <Coby@Coby-PC.T-mobile.com>
2017-10-11 23:41:36 -0400

    coby

--- zombieChatBot/src/zombieProgram/ZombieBotCoby.java
+++ zombieChatBot/src/zombieProgram/ZombieBotCoby.java
@@ -24,29 +24,29 @@ public class ZombieBotCoby implements Topic {
 	private int topicChosen;
 	private int dialogues = 0;
 	private int idkTime = 0;
-	
+	private int userWins = 0;
+	private int botWins = 0;
 	
 	
 	public ZombieBotCoby() {
 		String[] temp = {"conversation","chat","smalltalk"};
 		keywords = temp;
-		String[] temp1 = {"social life","hobbies","hobby","person","people","games","technology"};
+		String[] temp1 = {"social life","hobbies","hobby","person","people","games","game","technology"};
 		topicWords = temp1;
 		String[] temp2 = {"rock","paper","scissors"};
 		userOptions = temp2;
 		String[] temp3 = {"bye","goodbye","later","stop"};
 		goodbyeWords = temp3;
-		String[] temp4 = {"I don't...have a social life......all I do is sleepp.","We always have parties..we're already dead so we party all"
-				+ " night long without having to go to sleep.","Zerrrr I don't have.. any friends."};
+		String[] temp4 = {"I don't...have a social life......all I do is sleepppp.","We always have parties..we're already dead so we party all night long without having to go to sleep.","Zerrrr I don't have.. any friends.",
+				"Urrggg I haven't left my grave in urhh around 6 months..","I... urggh think I met a vampire yesterday.... or last uh week"};
 		socialResponses = temp4;
-		String[] temp5 = {"I like to eat BRAINS....durrrrr let me eat yours!","Zurrr I've been sleeping..using almost all my time.", "I like"
-				+ " to win as zombies in Plants Vs Zombies."};
+		String[] temp5 = {"I like to eat BRAINS....durrrrr let me eat yours!","Zurrr I've been sleeping..using almost all my time.", "I like to win as zombies in Plants Vs Zombies.","I.. like to walk around.. even if a few limbs fall off...",
+				"I liek to dance and dance...urgggg"};
 		hobbyResponses = temp5;
-		String[] temp6 = {"My favorite people are uh smart people.....their brains are rich and delicious.","Errrrr of course my least "
-				+ "favorite people are....dumb people."};
+		String[] temp6 = {"My favorite people are uh smart people.....their brains are rich and delicious.","Errrrr of course my least favorite people are....dumb people.","I was alive..when I think George....Wash Tin was president.."};
 		peopleResponses = temp6;
-		String[] temp7 = {"Technology? We don't use technology! When I was alive the only phone that was out was the first Nokia.", "I don't "
-				+ "know anything about today's technology."};
+		String[] temp7 = {"Technology....? What that.. we don't use technology... when me was alive the only phone that was out was the telegraph.", "I don't "
+				+ "know anything about today's technology....","Do the new.. technology.. create good food?"};
 		technologyResponses = temp7;
 		String[] temp8 = {"yes","ok","sure","okay","yeah"};
 		confirmation = temp8;
@@ -71,7 +71,7 @@ public class ZombieBotCoby implements Topic {
 	
 	@Override
 	public void startChatting(String response) {
-		ZombieBotMain.print("Hey. Let's have a conversation urrr! As you long as you feed me brainssssss afterwards. What do you want to talk about?");
+		ZombieBotMain.print("Hey. Let's have a conversation urrr... As you long as you feed me brainssssss afterwards. What do you want to talk about?");
 		chatting = true;
 		
 		while(chatting) {
@@ -91,13 +91,13 @@ public class ZombieBotCoby implements Topic {
 							ZombieBotMain.print(getResponse(topicChosen));
 						} else {
 							if(ZombieBotMain.findKeyword(response, secretWord, 0) >= 0) {
-								ZombieBotMain.print("I hate vampires! Zombies are much cooler.");
+								ZombieBotMain.print("I hate vampires.....Zombies are sooo much cooler.");
 							} else {
 								if(asktoSwitch && isthereWord(confirmation, response)) {
 									ZombieBotMain.chatbot.switchTopic(response, 2);
 								} else {
 									if(asktoSwitch && isthereWord(denial, response)) {
-										ZombieBotMain.print("Okay then. Let's keep talking.");
+										ZombieBotMain.print("Okay then..we keep talking.");
 										dialogues = 0;
 										asktoSwitch = false;
 									} else {
@@ -105,15 +105,15 @@ public class ZombieBotCoby implements Topic {
 										if(idkTime < 2) {	
 											ZombieBotMain.print("Errrr I don't get it. Can you say something else?");
 										} else {
-											ZombieBotMain.print("I don't get what you're saying. How about we talk about " + topicWords[(int) (Math.random()*topicWords.length)] + ".");
+											ZombieBotMain.print("I don't get what you're sayingit.....we talk about " + topicWords[(int) (Math.random()*topicWords.length)] + ". good?");
 										}
 									}
 								}
 							}
 						}
 						dialogues++;
-						if(dialogues > 4) {
-							ZombieBotMain.print("Now would you like to talk about my past life?");
+						if(dialogues > 5) {
+							ZombieBotMain.print("Now do you want...to talk urgggh about my past life?");
 							asktoSwitch = true;
 						}
 					}
@@ -146,22 +146,28 @@ public class ZombieBotCoby implements Topic {
 		playerChoice = player.toLowerCase();
 		gameStarted = false;
 		if(zombieChoice == 1 && playerChoice == "paper") {
-			return "I picked rock. You won.";
+			userWins++;
+			return "I picked rock....you won. The score is " + botWins + " me, " + userWins + " you.";
 		}
 		if(zombieChoice == 1 && playerChoice == "scissors") {
-			return "I picked rock. I win! Now let me eat your brain!";
+			botWins++;
+			return "I picked rock. I win so let me EAT your brainnn.. The score is " + botWins + " me, " + userWins + " you.";
 		}
 		if(zombieChoice == 2 && playerChoice == "rock") {
-			return "I picked paper. I win! Now let me eat your brain!";
+			botWins++;
+			return "I picked paper. I win! Now let me eat your brain! The score is hur" + botWins + " me, " + userWins + " you.";
 		}
 		if(zombieChoice == 2 && playerChoice == "scissors") {
-			return "I picked paper. You won.";
+			userWins++;
+			return "I picked paper. You won zurrrrr. The score... is " + botWins + " me, " + userWins + " you.";
 		}
 		if(zombieChoice == 3 && playerChoice == "rock") {
-			return "I picked scissors. You won.";
+			userWins++;
+			return "I picked scissors. Ughhhh You won.... The.. score is " + botWins + " me, " + userWins + " you.";
 		}
 		if(zombieChoice == 3 && playerChoice == "paper") {
-			return "I picked scissors. I won! Now let me eat your brain!";
+			botWins++;
+			return "Urrrrr I picked scissors. I won! Now let me eat your brain! The score is " + botWins + " me, " + userWins + " you.";
 		}
 		return "It's a tie!";
 	}
@@ -178,11 +184,11 @@ public class ZombieBotCoby implements Topic {
 		if(keywordFound == 3 || keywordFound == 4) {
 			return peopleResponses[(int) (Math.random()*peopleResponses.length)];
 		}
-		if(keywordFound == 5) {
+		if(keywordFound == 5 || keywordFound == 6) {
 			gameStarted = true;
-			return "I love games. Lets play rock, paper, scissors! Type in your choice";
+			return "I LIKE GAMES. we play rock, paper, scissors? Uhhh type your choice";
 		}
-		if(keywordFound == 6) {
+		if(keywordFound == 7) {
 			return technologyResponses[(int) (Math.random()*technologyResponses.length)];
 		}
 		return "";
