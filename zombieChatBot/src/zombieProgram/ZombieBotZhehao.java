65ea71d7118a59c353fdc6572f963314a066bb8d
zheha <zheha@DESKTOP-SNC7R9G.home>
2017-10-11 22:04:58 -0400

    works

--- zombieChatBot/src/zombieProgram/ZombieBotZhehao.java
+++ zombieChatBot/src/zombieProgram/ZombieBotZhehao.java
@@ -11,7 +11,11 @@ public class ZombieBotZhehao implements Topic {
 	private String[] happyresponse;
 	private String[] suggesttopic;
 	private String[] asktostop;
+	private String[] compliments;
 	private String[] confirm;// put keywords in
+	private String[] angerlevel2;
+	private String[] angerlevel3;
+	private int repeatnonsense=0;
 	private int questionasked=0;
 	private boolean wanttochange = false;
 	private boolean ischatted = false;
@@ -37,14 +41,20 @@ public class ZombieBotZhehao implements Topic {
 				"Errr They used to called me Carl Mike o Jack sin. I was tee bes dancer the worid had ever seen!"};
 		happyresponse= temphappyresponse;
 		String[] tempasktostop= {"That is not nice!Don't said that!","IM GOING TO GET MAD!","YOU BETTER SAID SORRY!","If You trash talk one more time Im goint to end u!",
-			"hi"	};
+			"goodbye dont wanna talk to you anymore."};
 		asktostop = tempasktostop;
-		String[] tempsuggesttopic = {"I don't understand you. Let's talk about my hobbies.","ERR wat? I can't answer that. How about we talk about school?","Do you want to talk about something else?Maybe like my personal legacy.(hint: background)",
+		String[] tempsuggesttopic = {"I don't understand you. Let's talk about my hobbies.","ERR wat? I can't answer that. How about we talk about school?","IDK","Do you want to talk about something else?Maybe like my personal legacy.(hint: background)",
 				"I don't understand! Say something else!"};
 		suggesttopic=tempsuggesttopic;
 		String[] tempothertopic= {"ok","sure","okay","yes"};
 		confirm= tempothertopic;
-		secretWord = "Dora";
+		String[] tempcompliment= {"amazeing","cool","fantastic","great","awesome","good job","sorry"};
+		compliments=tempcompliment;
+		String[] tempangerlevel2= {"Dancing","I like going to school.","1","I was a dancer."};
+		angerlevel2=tempangerlevel2;
+		String[] tempangerlevel3= {"I won't answer your question unless you apologize","APOLOGIZE","Yo stop talking trash.","Im going to eat you."};
+		angerlevel3=tempangerlevel3;
+		secretWord = "Dora the explorer";
 	}
 	public boolean isTriggered(String response) {
 		for(int i=0; i<keywords.length;i++)
@@ -56,15 +66,14 @@ public class ZombieBotZhehao implements Topic {
 		}
 		return false;
 	}
-	public boolean sademotiontriggered(String response)
+	public boolean keywordtriggered(String response, String[] arr)
 	{
-		for(int x=0;x<angerkeywords.length;x++)
+		for(int i=0;i<arr.length;i++)
 		{
-			if(ZombieBotMain.findKeyword(response, angerkeywords[x],0)>=0)
+			if(ZombieBotMain.findKeyword(response,arr[i],0) >= 0)
 			{
 				return true;
 			}
-			
 		}
 		return false;
 	}
@@ -80,19 +89,6 @@ public class ZombieBotZhehao implements Topic {
 			
 		}
 		return false;
-	}
-	public boolean triggerothertopic(String response)
-	{
-		for(int i=0;i<confirm.length;i++)
-		{
-			if(ZombieBotMain.findKeyword(response,confirm[i],0) >= 0)
-			{
-				return true;
-			}
-			
-		}
-		return false;
-		
 	}
 	public boolean haschatted()
 	{
@@ -116,58 +112,86 @@ public class ZombieBotZhehao implements Topic {
 			}
 			if(wanttochange == true)
 			{
-				if(triggerothertopic(response) == true)
+				if(keywordtriggered(response,confirm) == true)
 				{
 					ZombieBotMain.chatbot.switchTopic(response,3);
 				}
 				ZombieBotMain.print("OK let talk more about my past life.");
+				wanttochange=false;
 				questionasked= 0;
-			}
-			
-			if (ZombieBotMain.findKeyword(response, secretWord, 0)>=0) {
-				ZombieBotMain.print("ERRR! DERRRRRRR! Me love Doraaaaa! DERRR! You can be me new bes friend!");
-				angercount=0;
-			}
-			if(sademotiontriggered(response))
-			{
-				
-				angercount+=1;
-				questionasked +=1;
-				ZombieBotMain.print(asktostop[angercount]);
-				if(angercount == 9)
-				{
-					ZombieBotMain.print("ERRRRRRRRRR! That is it! I'm going to eat your brain!CARL DONT WANNA TALK YOU YOU ANYMORE! FORCE SHUTDOWN!");
-					ZombieBotMain.chatbot.startTalking();
-				}
-			}else if(talkingpointisfound(response))
-			{
-				if(angercount<4)
-				{
-					questionasked +=1;
-					ZombieBotMain.print(happyresponse[trackingnumber]);
-					
-				}
-				else
-					{
-						questionasked +=1;
-					 	ZombieBotMain.print(angryresponse[trackingnumber]);
-					}
 			}else {
-				questionasked+=1;
-				ZombieBotMain.randomText(suggesttopic);
+				if(keywordtriggered(response, compliments) == true)
+				{
+					if(angercount>0)
+					{
+						angercount--;
+					}
 				
-			}
-			if(questionasked > 4)
-			{
-				ZombieBotMain.print("Do you want to talk about my zombie life?");
-				wanttochange = true;
+					ZombieBotMain.print("That is nice of you.");
+				}else {
+					if (ZombieBotMain.findKeyword(response, secretWord, 0)>=0) {
+						ZombieBotMain.print("ERRR! DERRRRRRR! Me love Doraaaaa! DERRR! You can be me new bes friend!");
+						angercount=0;
+					}else {
+						if(keywordtriggered(response,angerkeywords) == true)
+						{
 				
+							angercount+=1;
+							questionasked +=1;
+							ZombieBotMain.print(asktostop[angercount]);
+							if(angercount == 12)
+							{
+								ZombieBotMain.print("ERRRRRRRRRR! That is it! I'm going to eat your brain!CARL DONT WANNA TALK YOU YOU ANYMORE! FORCE SHUTDOWN!");
+								ZombieBotMain.chatbot.startTalking();
+							}
+						}else if(talkingpointisfound(response))
+						{
+							if(angercount==0)
+							{
+								questionasked +=1;
+								ZombieBotMain.print(happyresponse[trackingnumber]);
+					
+							}else
+							{
+								questionasked +=1;
+								if(angercount>=3 && angercount<6)
+								{
+									ZombieBotMain.print(angryresponse[trackingnumber]);
+								}
+								else
+								{
+									if(angercount>=6 && angercount<8)
+									{
+										ZombieBotMain.print(angerlevel2[trackingnumber]);
+									}
+										ZombieBotMain.print(angerlevel3[trackingnumber]);
+								}
+								
+								
+							}
+						}else {
+							questionasked+=1;
+							ZombieBotMain.randomText(suggesttopic);
+							repeatnonsense+=1;
+							if(repeatnonsense>=3)
+							{
+								angercount+=1;
+								repeatnonsense=0;
+							}
+						}
+						if(questionasked > 4)
+						{
+							ZombieBotMain.print("Do you want to talk about my zombie life?");
+							wanttochange = true;
+				
+						}
+		
+				}
+			
+			
+		
+				}
 			}
-		
-			
-			
-			
-		
 		}
 	}
 
